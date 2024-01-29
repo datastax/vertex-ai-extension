00b473ee59c851fd375b2e97dc799c07e0803daa
Eric Hare <ericrhare@gmail.com>
2024-01-29 09:54:12 -0700

    Endpoint restructuring for new extensions

--- astra-crud-extension/extension.py
+++ astra-crud-extension/extension.py
@@ -1,9 +1,35 @@
 from flask import Flask, jsonify, request
 
+from google.cloud import secretmanager
+
 from astrapy.db import AstraDB
 
 import os
 import uuid
+
+# Set the path to the service account file
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "integrations-379317-480fb9eb65d2.json"
+
+# Provide the GCP Project ID
+project_id = os.environ.get("PROJECT_ID", "747469159044")
+
+# Access the secret client
+secret_client = secretmanager.SecretManagerServiceClient()
+
+# Next, access the secret for the Astra Token
+secret_id = "ASTRA_DB_APPLICATION_TOKEN"
+version_id = 1
+secret_name = f'projects/{project_id}/secrets/{secret_id}/versions/{version_id}'
+response = secret_client.access_secret_version(request={"name": secret_name})
+DEFAULT_TOKEN = response.payload.data.decode("UTF-8")
+
+# Next, access the secret for the Astra DB Endpoint
+secret_id = "ASTRA_DB_API_ENDPOINT"
+version_id = 1
+secret_name = f'projects/{project_id}/secrets/{secret_id}/versions/{version_id}'
+response = secret_client.access_secret_version(request={"name": secret_name})
+DEFAULT_API_ENDPOINT = response.payload.data.decode("UTF-8")
+
 
 app = Flask(__name__)
 
@@ -20,8 +46,8 @@ def read_astra():
     params = request.json
 
     # Grab the Astra token and api endpoint from the environment
-    token = params.get("token")
-    api_endpoint = params.get("api_endpoint")
+    token = params.get("token", DEFAULT_TOKEN)
+    api_endpoint = params.get("api_endpoint", DEFAULT_API_ENDPOINT)
 
     # Error out if we don't have the token or api_endpoint
     if not token or not api_endpoint:
@@ -44,8 +70,8 @@ def insert_astra():
     params = request.json
 
     # Grab the Astra token and api endpoint from the environment
-    token = params.get("token")
-    api_endpoint = params.get("api_endpoint")
+    token = params.get("token", DEFAULT_TOKEN)
+    api_endpoint = params.get("api_endpoint", DEFAULT_API_ENDPOINT)
 
     # Error out if we don't have the token or api_endpoint
     if not token or not api_endpoint:
@@ -78,8 +104,8 @@ def update_astra():
     params = request.json
 
     # Grab the Astra token and api endpoint from the environment
-    token = params.get("token")
-    api_endpoint = params.get("api_endpoint")
+    token = params.get("token", DEFAULT_TOKEN)
+    api_endpoint = params.get("api_endpoint", DEFAULT_API_ENDPOINT)
 
     # Error out if we don't have the token or api_endpoint
     if not token or not api_endpoint:
@@ -103,8 +129,8 @@ def delete_astra():
     params = request.json
 
     # Grab the Astra token and api endpoint from the environment
-    token = params.get("token")
-    api_endpoint = params.get("api_endpoint")
+    token = params.get("token", DEFAULT_TOKEN)
+    api_endpoint = params.get("api_endpoint", DEFAULT_API_ENDPOINT)
 
     # Error out if we don't have the token or api_endpoint
     if not token or not api_endpoint:
