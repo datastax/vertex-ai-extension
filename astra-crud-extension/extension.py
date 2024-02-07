634d9e6dbc4aca2e1483779ee472fe9130fc623d
Eric Hare <ericrhare@gmail.com>
2024-02-06 18:18:55 -0700

    Updates for API auth!

--- astra-crud-extension/extension.py
+++ astra-crud-extension/extension.py
@@ -1,42 +1,17 @@
 from flask import Flask, jsonify, request
 
-from google.cloud import secretmanager
-
 from astrapy.db import AstraDB
 
 import os
 import uuid
 
-# Set the path to the service account file
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "integrations-379317-480fb9eb65d2.json"
-
-# Provide the GCP Project ID
-project_id = os.environ.get("PROJECT_ID", "747469159044")
-
-# Access the secret client
-secret_client = secretmanager.SecretManagerServiceClient()
-
-# Next, access the secret for the Astra Token
-secret_id = "ASTRA_DB_APPLICATION_TOKEN"
-version_id = 1
-secret_name = f'projects/{project_id}/secrets/{secret_id}/versions/{version_id}'
-response = secret_client.access_secret_version(request={"name": secret_name})
-DEFAULT_TOKEN = response.payload.data.decode("UTF-8")
-
-# Next, access the secret for the Astra DB Endpoint
-secret_id = "ASTRA_DB_API_ENDPOINT"
-version_id = 1
-secret_name = f'projects/{project_id}/secrets/{secret_id}/versions/{version_id}'
-response = secret_client.access_secret_version(request={"name": secret_name})
-DEFAULT_API_ENDPOINT = response.payload.data.decode("UTF-8")
-
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/hello", methods=["GET"])
 def hello_world():
-    data = {"message": "Hello, World!"}
+    data = {"message": request.headers.get("token")}
     
     return jsonify(data)
 
@@ -46,12 +21,12 @@ def read_astra():
     params = request.json
 
     # Grab the Astra token and api endpoint from the environment
-    token = params.get("token", DEFAULT_TOKEN)
-    api_endpoint = params.get("api_endpoint", DEFAULT_API_ENDPOINT)
+    token = params.get("token", request.headers.get("token"))
+    api_endpoint = params.get("api_endpoint")
 
     # Error out if we don't have the token or api_endpoint
-    if not token or not api_endpoint:
-        return jsonify({"error": "token or api_endpoint not provided"})
+    if not api_endpoint:
+        return jsonify({"error": "api_endpoint not provided"})
 
     # Optional Params for the astra call
     table = params.get("tableName", "test")
@@ -70,12 +45,12 @@ def insert_astra():
     params = request.json
 
     # Grab the Astra token and api endpoint from the environment
-    token = params.get("token", DEFAULT_TOKEN)
-    api_endpoint = params.get("api_endpoint", DEFAULT_API_ENDPOINT)
+    token = params.get("token", request.headers.get("token"))
+    api_endpoint = params.get("api_endpoint")
 
     # Error out if we don't have the token or api_endpoint
-    if not token or not api_endpoint:
-        return jsonify({"error": "token or api_endpoint not provided"})
+    if not api_endpoint:
+        return jsonify({"error": "api_endpoint not provided"})
 
     # Some example data
     doc = {
@@ -104,12 +79,12 @@ def update_astra():
     params = request.json
 
     # Grab the Astra token and api endpoint from the environment
-    token = params.get("token", DEFAULT_TOKEN)
-    api_endpoint = params.get("api_endpoint", DEFAULT_API_ENDPOINT)
+    token = params.get("token", request.headers.get("token"))
+    api_endpoint = params.get("api_endpoint")
 
     # Error out if we don't have the token or api_endpoint
-    if not token or not api_endpoint:
-        return jsonify({"error": "token or api_endpoint not provided"})
+    if not api_endpoint:
+        return jsonify({"error": "api_endpoint not provided"})
 
     # Optional Params for the astra call
     table = params.get("tableName", "test")
@@ -129,12 +104,12 @@ def delete_astra():
     params = request.json
 
     # Grab the Astra token and api endpoint from the environment
-    token = params.get("token", DEFAULT_TOKEN)
-    api_endpoint = params.get("api_endpoint", DEFAULT_API_ENDPOINT)
+    token = params.get("token", request.headers.get("token"))
+    api_endpoint = params.get("api_endpoint")
 
     # Error out if we don't have the token or api_endpoint
-    if not token or not api_endpoint:
-        return jsonify({"error": "token or api_endpoint not provided"})
+    if not api_endpoint:
+        return jsonify({"error": "api_endpoint not provided"})
 
     # Optional Params for the astra call
     table = params.get("tableName", "test")
